512c14d72b1b209ec7b17bad57c48ce7433b2ae3
WMwalysson <202502092041@alunos.estacio.br>
2025-02-22 05:02:16 +0000

    tudo certo

---
+++ Manipulação e conversão de tipos de dados/#include <stdio.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    float b = 3;
+    float quociente = (float) a / b; // 'a' é  explicitamente convertido para float
+    
+    printf("quociente: %.2f\n", quociente);
+    
+    return 0;
+
+    
+
+
+
+
+}
